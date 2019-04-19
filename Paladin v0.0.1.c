0cdb677083bffbe5f0fe77eed16d444873174b80
morsdeathlord69 <morsdeathlord@gmail.com>
2019-04-19 16:51:01 -0400

    Create Paladin v0.0.1.c

---
+++ Paladin v0.0.1.c
@@ -0,0 +1,30 @@
+//CNIT 31500 Final Group Project
+//Vivian Phan
+//Peter Shoaf
+//Lancia Raja
+//Shyam Merchant
+
+#include <stdio.h>
+#include <string.h>
+
+struct Person {
+    int id;
+    char *first_name;
+    char *last_name;
+    char *userName;
+    char *password;
+    struct node *next;
+}node;
+
+int main()
+{
+    printf("Welcome to Fortress.\n");
+    printf("Do you have an account? (Y/N)\n");
+    
+    return 0;
+}
+
+typedef struct Node
+{
+    //char* 
+}
